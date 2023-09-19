e0d5323897231c6bb8b3c28bb826ec499a62b4bf
cherry <shps950421@gmail.com>
2023-09-19 15:37:08 +0800

    finish
    finished version

---
+++ src/main/java/com/connectu/connectuapi/ConnectUApiApplication.java
@@ -0,0 +1,10 @@
+package com.connectu.connectuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ConnectUApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectUApiApplication.class, args);
+    }
+}
